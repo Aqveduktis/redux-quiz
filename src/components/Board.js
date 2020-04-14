0ac84dd3bece62a6ee510327d5c114e8adc8a587
Aqveduktis <57035659+Aqveduktis@users.noreply.github.com>
2020-04-14 17:38:37 +0200

    Initial commit

---
+++ src/components/Board.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Square } from 'components/Square'
+import {useSelector} from 'react-redux'
+
+export const Board = () => {
+  // TODO - use the 'squares' state from the game reducer
+  const squares = useSelector((store)=>(store.game.squares))
+
+  return (
+    <div className="board">
+      {squares.map((value, index) => (
+        <Square key={index} value={value} index={index} />
+      ))}
+    </div>
+  )
+}
---
+++ src/components/Square.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import { Player } from 'components/Player'
+import {game} from 'reducers/game'
+
+export const Square = ({ value, index }) => {
+  const dispatch = useDispatch()
+  const handleClick = () => {
+    // TODO send the captureSquare action
+    dispatch(game.actions.captureSquare({index}))
+  }
+
+  return (
+    <button
+      className={value === null ? 'square clickable' : 'square'}
+      type="button"
+      onClick={handleClick}>
+      <Player value={value} />
+    </button>
+  )
+}
